e2274478a07d64ef6b3159ff96bde577df620604
emmanueladegor <emmanueladegor2@gmail.com>
2021-01-14 13:09:14 +0100

    Added Functionalities for CRUD including DTOS

---
+++ Dotnet-rpg/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AutoMapper;
+using Dotnet_rpg.Dtos.Character;
+using Dotnet_rpg.Models;
+
+namespace Dotnet_rpg
+{
+    public class AutoMapperProfile  : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character, GetCharacterDto>();
+            CreateMap<AddCharacterDto, Character>();
+        }
+    }
+}
+ 
