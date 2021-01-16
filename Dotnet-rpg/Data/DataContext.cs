e825cd1839f8c9ad6e295d626dce90f185882cf1
emmanueladegor <emmanueladegor2@gmail.com>
2021-01-16 16:31:05 +0100

    Added MSSQL CharacterTable and UsersTable Database

---
+++ Dotnet-rpg/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_rpg.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Character> Characters { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Dotnet-rpg/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Dotnet_rpg.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
